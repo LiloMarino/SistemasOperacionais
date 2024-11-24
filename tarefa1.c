b94442288128db99ca6e6fa307321289e8507de5
LiloMarino <lilo.marino@hotmail.com>
2024-11-23 22:27:02 -0300

    feat: :sparkles: Feito tarefa dia 22

--- tarefa1.c
+++ tarefa1.c
@@ -11,7 +11,7 @@
 #define ASCII_MAX 126
 #define PASSWORD_LEN 12
 
-void bruteForce(int start) {
+void bruteForce(int start, int writePipe, bool usePipe) {
     char attempt[4] = {0};
     for (int i = ASCII_MIN; i <= ASCII_MAX; i++) {
         for (int j = ASCII_MIN; j <= ASCII_MAX; j++) {
@@ -20,8 +20,14 @@ void bruteForce(int start) {
                 attempt[1] = j;
                 attempt[2] = k;
                 if (checkPassword(attempt, start) == 0) {
-                    printf("Processo %d com pai %d encontrou %d-%d: %s\n",
-                           getpid(), getppid(), start, start + 2, attempt);
+                    if (usePipe) {
+                        // Envia a senha encontrada para o processo pai pelo pipe
+                        write(writePipe, attempt, sizeof(attempt));
+                        close(writePipe);
+                    } else {
+                        // Imprime diretamente no terminal
+                        printf("Senha encontrada: %s (range: %d-%d)\n", attempt, start, start + 2);
+                    }
                     return;
                 }
             }
@@ -39,13 +45,32 @@ int main(int argc, char *argv[]) {
 
     printf("Processo %d com pai %d quebrando a senha...\n", getpid(), getppid());
     if (useFork) {
-        // Com fork, para cada filho criado, ele irá quebrar uma parte da senha
+        int pipes[4][2]; // Um pipe para cada processo filho
+
+        // Cria os pipes
+        for (int i = 0; i < 4; i++) {
+            if (pipe(pipes[i]) == -1) {
+                perror("Erro ao criar pipe");
+                return EXIT_FAILURE;
+            }
+        }
+
+        // Criação dos processos filhos
         for (int i = 0; i < 4; i++) {
             pid_t pid = fork();
-            // Verifica se conseguiu criar o processo filho
-            if (pid == 0) {
-                bruteForce(i * 3);
+            if (pid == -1) {
+                perror("Erro ao criar processo filho");
+                return EXIT_FAILURE;
+            }
+
+            if (pid == 0) { 
+                // Código do processo filho
+                close(pipes[i][0]);
+                bruteForce(i * 3, pipes[i][1], true);
                 exit(0);
+            } else { 
+                // Código do processo pai
+                close(pipes[i][1]);
             }
         }
 
@@ -53,12 +78,23 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < 4; i++) {
             wait(NULL);
         }
+        
+        // O pai lê as respostas dos filhos
+        char password[4] = {0};
+        for (int i = 0; i < 4; i++) {
+            ssize_t bytesRead = read(pipes[i][0], password, sizeof(password));
+            if (bytesRead > 0) {
+                printf("Processo pai recebeu a senha: %s\n", password);
+            }
+            close(pipes[i][0]);
+        }
+
     } else {
         // Sem fork, testar todas as partes da senha sequencialmente
         for (int i = 0; i < 4; i++) {
-            bruteForce(i * 3);
+            bruteForce(i * 3, -1, false);
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
