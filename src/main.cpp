12eed047a1ea9ea6fbb0e46845b682d453d79913
LiloMarino <lilo.marino@hotmail.com>
2025-02-07 13:57:09 -0300

    feat: :sparkles: Adicionado leitura linha a linha do addresses

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
     std::ifstream backing_store("BACKING_STORE.bin", std::ios::binary);
-    if (!backing_store) {
-        std::cerr << "Erro ao abrir o arquivo!" << std::endl;
+    if (!backing_store)
+    {
+        std::cerr << "Erro ao abrir o arquivo BACKING_STORE.bin!" << std::endl;
         return 1;
     }
 
+    std::ifstream addresses("addresses.txt");
+    if (!addresses)
+    {
+        std::cerr << "Erro ao abrir o arquivo addresses.txt!" << std::endl;
+        return 1;
+    }
     
+    std::string linha;
+    while (std::getline(addresses, linha))
+    {
+        int address = std::stoi(linha);
+    }
 }
