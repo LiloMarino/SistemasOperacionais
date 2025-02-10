7fad1ba626bc9d43e6f0cb2fbdc1ffe07609303f
LiloMarino <lilo.marino@hotmail.com>
2025-02-10 09:45:11 -0300

    feat: :sparkles: Implementado função de bits

--- src/binary.hpp
+++ src/binary.hpp
@@ -2,6 +2,8 @@
 #define BINARY_HPP
 
 #include <fstream>
+#include <bitset>
+#include <iostream>
 #include "page.hpp"
 
 namespace binary
@@ -13,6 +15,45 @@ namespace binary
      * @return Retorna a página
      */
     Page readPage(int page_number, std::ifstream &file);
+
+    /**
+     * @brief Obtém os n bits menos significativos de um número.
+     *
+     * @tparam T Tipo de retorno desejado (ex: uint8_t, uint16_t, uint32_t).
+     * @param num Número de entrada.
+     * @param n Quantidade de bits a serem extraídos.
+     * @return T Valor contendo os n bits menos significativos.
+     */
+    template <typename T>
+    T get_low_bits(T num, uint32_t n)
+    {
+        return static_cast<T>(num & ((1U << n) - 1));
+    }
+
+    /**
+     * @brief Obtém os n bits mais significativos de um número.
+     *
+     * @tparam T Tipo de retorno desejado (ex: uint8_t, uint16_t, uint32_t).
+     * @param num Número de entrada.
+     * @param n Quantidade de bits a serem extraídos.
+     * @return T Valor contendo os n bits mais significativos.
+     */
+    template <typename T>
+    T get_high_bits(T num, uint32_t n)
+    {
+        return static_cast<T>(num >> (sizeof(T) * 8 - n));
+    }
+
+    /**
+     * @brief Imprime o número em formato binário.
+     *
+     * @param num Número a ser impresso.
+     */
+    template <typename T>
+    void print_binary(T num)
+    {
+        std::cout << "0b" << std::bitset<sizeof(T) * 8>(num) << std::endl;
+    }
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "binary.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -17,10 +18,12 @@ int main(int argc, char const *argv[])
         std::cerr << "Erro ao abrir o arquivo addresses.txt!" << std::endl;
         return 1;
     }
-    
-    std::string linha;
-    while (std::getline(addresses, linha))
-    {
-        int address = std::stoi(linha);
-    }
+
+    uint32_t address;
+    addresses >> address;
+
+    binary::print_binary(address);
+    binary::print_binary(binary::get_low_bits<uint16_t>(address, 16));
+    binary::print_binary(binary::get_low_bits<uint8_t>(binary::get_low_bits<uint16_t>(address, 16), 8));
+    binary::print_binary(binary::get_high_bits<uint8_t>(binary::get_low_bits<uint16_t>(address, 16), 8));
 }
